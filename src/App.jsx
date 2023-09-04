c32e4be91f765e372b5210fc42ad7037e408e3ae
engineerahme <ahmedsalah523232@gmail.com>
2023-09-04 13:08:37 +0300

    lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [person, changeName] = useState("sara");
+  const [age, setAge] = useState("28")
+
+
+
+  const [count, setColor] = useState(0)
 
   return (
     <>
     
-      
+      <div>
+         <h2>My name is {person}</h2>
+         <button onClick={() => { changeName("Sally") }}>Change Name </button>
+      </div>
+
+      <br />
+
+      <div>
+         <h2>My age is {age} </h2>
+         <button onClick={() => { setAge("30") }}>Change Name  </button>
+      </div>
+
+      <br />
+      <br />
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
