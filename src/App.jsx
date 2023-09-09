c7506982e5f5cf1e646835558e708371eff51561
engineerahme <ahmedsalah523232@gmail.com>
2023-09-09 15:30:05 +0300

    2.3 lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+import './theme.css';
 
 import './App.css'
 
 function App() {
   const [person, changeName] = useState("sara");
   const [age, setAge] = useState("28")
-
-
-
   const [count, setCount] = useState(0)
 
+  const [theme, settheme] = useState("");
+
+
   return (
-    <>
-    
+    <div  className={` App ${theme}`}>
+    <div>
+      <button onClick={() => { settheme("light")}} style={{marginRight:"26px"}}>light</button>
+      <button  onClick={() => { settheme("dark")}} style={{marginRight:"26px"}}>dark</button>
+      <button  onClick={() => { settheme("grey")}} style={{marginRight:"26px"}}>gray</button>
+      <button  onClick={() => { settheme("pink")}}>pink</button>
+    </div>
+
       <div>
-         <h2>My name is {person}</h2>
+         <h2 style={{marginTop:"66px"}}>My name is {person}</h2>
          <button onClick={() => { changeName("Sally") }}>Change Name </button>
       </div>
 
@@ -32,7 +40,7 @@ function App() {
           count is {count}
         </button>
       
-    </>
+    </div>
   )
 }
 
